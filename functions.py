eadf38b24c35acc5a40288b1ee3812692b4aa25f
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-21 17:08:44 -0700

    "Added new settings and a mock up quiz"

---
+++ functions.py
@@ -0,0 +1,21 @@
+# 1. Falsy
+
+# 2. Nothing, unless you used print to wrap it all
+
+# 3. 1 would be the last printed value
+
+# 4. tuples are defined using parentheses, and they are immutable. Treat them like a const array. Where List is muttable and can be changed after definition / initializaition
+
+# 5. Objects
+
+# 6. A list comprehension allows you to create a new list by applying an expression or a set result to happen to a value that matches the condition
+
+# example: list_dos = [this_expression_to_happen for item in items if condition]
+
+# 7. True
+
+# 8. It won't work because long_term_debt comes after *args, and args ca be any number ov values within a tuple, or list. It will never reach long_term_debt.
+
+# 9. __init__ is called when a new instance of a class is created. Think of __init__ like the constructor function that comes prebuilt within classes in Javascript, and other languages.
+
+# 10. [0, 2, 4]
